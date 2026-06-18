0216d5525f4d222486235d3efd30bd21a195381d
Jordi Diaz <Jordi@Mehmets-MacBook-Pro.local>
2026-06-18 16:07:36 +0200

    Fix technical SEO metadata

---
+++ js/google-ads-tracking.js
@@ -0,0 +1,16 @@
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+
+gtag('set', 'linker', {
+  domains: [
+    'voyagerballoons.eu',
+    'www.voyagerballoons.eu',
+    'shop.voyagerballoons.eu'
+  ],
+  accept_incoming: true,
+  decorate_forms: true
+});
+
+gtag('js', new Date());
+gtag('config', 'GT-55NTF5CN');
+gtag('config', 'AW-11564692382');
